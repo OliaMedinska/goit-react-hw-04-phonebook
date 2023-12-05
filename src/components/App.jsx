6e87abd658f23901e6ba2916b08cb44afc4d9711
OliaMedinska <medinska2@gmail.com>
2023-12-05 01:52:44 +0100

    app refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,96 +1,82 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Form } from './Form/Form';
 import { Contact } from './Contact/Contact';
 import { Filter } from './Filter/Filter';
-import { Component } from 'react';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      window.localStorage.setItem(
-        'contacts',
-        JSON.stringify({ contacts: this.state.contacts })
-      );
-    }
-  }
-
-  componentDidMount() {
-    const savedContacts = JSON.parse(window.localStorage.getItem('contacts'));
+  useEffect(() => {
+    const savedContacts =
+      JSON.parse(window.localStorage.getItem('contacts')) || [];
     if (savedContacts) {
-      this.setState({ contacts: savedContacts.contacts });
+      setContacts(savedContacts);
     }
-  }
+  }, []);
 
-  formSubmitHandler = ({ name, number }) => {
-    if (this.checkContactName(name)) {
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const formSubmitHandler = ({ name, number }) => {
+    if (checkContactName(name)) {
       alert(`${name} is already in contacts.`);
       return;
     }
 
     const id = nanoid();
-    this.setState(({ contacts }) => ({
-      contacts: [
-        ...contacts,
-        {
-          name,
-          number,
-          id,
-        },
-      ],
-    }));
+    setContacts(contacts => [
+      ...contacts,
+      {
+        name,
+        number,
+        id,
+      },
+    ]);
   };
 
-  changeFilter = evt => {
-    this.setState({ filter: evt.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleContacts = () => {
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  checkContactName = q => {
-    const { contacts } = this.state;
+  const checkContactName = q => {
     return contacts.some(({ name }) => name.toLowerCase() === q.toLowerCase());
   };
 
-  deleteContact = deleteId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(item => item.id !== deleteId),
-    }));
+  const deleteContact = deleteId => {
+    setContacts(prevState =>
+      prevState.contacts.filter(item => item.id !== deleteId)
+    );
   };
 
-  render() {
-    return (
-      <div className="content">
-        <h1 className="heading">Phonebook</h1>
-        <Form onSubmit={this.formSubmitHandler}></Form>
-        <h2 className="heading">Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.changeFilter}></Filter>
-        {this.getVisibleContacts().length !== 0 ? (
-          <Contact
-            contactList={this.getVisibleContacts()}
-            handleDeleteContact={this.deleteContact}
-          ></Contact>
-        ) : (
-          <p className="heading">No contacts</p>
-        )}
-      </div>
-    );
-  }
-}
-
-export default App;
+  return (
+    <div className="content">
+      <h1 className="heading">Phonebook</h1>
+      <Form onSubmit={formSubmitHandler}></Form>
+      <h2 className="heading">Contacts</h2>
+      <Filter value={filter} onChange={changeFilter}></Filter>
+      {getVisibleContacts().length !== 0 ? (
+        <Contact
+          contactList={getVisibleContacts()}
+          handleDeleteContact={deleteContact}
+        ></Contact>
+      ) : (
+        <p className="heading">No contacts</p>
+      )}
+    </div>
+  );
+};
